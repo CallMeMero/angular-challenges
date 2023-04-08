32fb9847c8c8bdcf3bc031679b59db7d9f9e76b0
thomas <thomas.ext@squadracer.com>
2023-04-08 21:56:17 +0200

    feat(tl): add solution for testing with tl

--- apps/testing-nested/jest.config.ts
+++ apps/testing-nested/jest.config.ts
@@ -3,7 +3,6 @@ export default {
   displayName: 'testing-nested',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/testing-nested',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
--- apps/testing-nested/src/app/child.component.spec.ts
+++ apps/testing-nested/src/app/child.component.spec.ts
@@ -1,12 +1,53 @@
-import { render } from '@testing-library/angular';
+import { fireEvent, render, screen } from '@testing-library/angular';
+import { createMock } from '@testing-library/angular/jest-utils';
+import userEvent from '@testing-library/user-event';
 import { ChildComponent } from './child.component';
-
+import { HttpService } from './http.service';
 describe('ChildComponent', () => {
+  const setup = async () => {
+    const httpService = createMock(HttpService);
+    httpService.sendTitle = jest.fn();
+
+    await render(ChildComponent, {
+      componentProviders: [
+        {
+          provide: HttpService,
+          useValue: httpService,
+        },
+      ],
+    });
+    const user = userEvent.setup();
+    return { user, httpService };
+  };
+
   test('add Good title and send request title with no error', async () => {
-    await render(ChildComponent);
+    const { user, httpService } = await setup();
+
+    const validateButton = screen.getByRole('button', { name: /validate/i });
+    const input = screen.getByRole('textbox');
+
+    expect(input).toHaveTextContent('');
+
+    await user.type(input, 'Good');
+
+    expect(screen.queryByText('Title is Good')).toBeInTheDocument();
+
+    fireEvent.click(validateButton);
+
+    expect(screen.queryByText('Title is required !!!')).not.toBeInTheDocument();
+    expect(httpService.sendTitle).toHaveBeenCalledTimes(1);
   });
 
   test('fail validating title because no title were typed', async () => {
-    await render(ChildComponent);
+    const { httpService } = await setup();
+
+    const validateButton = screen.getByRole('button', { name: /validate/i });
+
+    expect(screen.queryByText('Title is')).toBeInTheDocument();
+
+    fireEvent.click(validateButton);
+
+    expect(screen.queryByText('Title is required !!!')).toBeInTheDocument();
+    expect(httpService.sendTitle).not.toHaveBeenCalled();
   });
 });
