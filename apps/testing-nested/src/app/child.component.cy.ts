a4c5556eb4f58f252e20a5119353a43a4f4829a6
thomas <thomas.ext@squadracer.com>
2023-04-08 14:18:49 +0200

    feat(test): add challenge 18 and 19 avout testing

--- apps/testing-nested/src/app/child.component.cy.ts
+++ apps/testing-nested/src/app/child.component.cy.ts
@@ -1,15 +1,33 @@
+import { TestBed } from '@angular/core/testing';
 import { ChildComponent } from './child.component';
+import { HttpService } from './http.service';
 
 describe(ChildComponent.name, () => {
   const setup = () => {
-    cy.mount(ChildComponent);
+    cy.mount(ChildComponent).then(() => {
+      const http = TestBed.inject(HttpService);
+      cy.stub(http, 'sendTitle').as('http');
+    });
   };
 
   test('add Good title and send request title with no error', () => {
     setup();
+
+    cy.get("input[type='text']").type('Good');
+    cy.get('button').click();
+
+    cy.contains('Title is required !!!').should('not.exist');
+
+    cy.get('@http').should('be.called');
   });
 
   test('fail validating title because no title were typed', () => {
     setup();
+
+    cy.get('button').click();
+
+    cy.contains('Title is required !!!');
+
+    cy.get('@http').should('not.be.called');
   });
 });
