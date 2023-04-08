91aff0b1814054828d61c6bd2c9fc7711f7e753f
thomas <thomas.ext@squadracer.com>
2023-04-08 13:40:32 +0200

    feat(cy): add cypress test for router outlet testing

--- apps/testing-router-outlet/src/app/app.component.cy.ts
+++ apps/testing-router-outlet/src/app/app.component.cy.ts
@@ -1,23 +1,62 @@
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 
 describe(AppComponent.name, () => {
+  const setup = () => {
+    cy.mount(AppComponent, {
+      providers: [provideRouter(appRoutes)],
+    });
+    cy.get('nav a:first').click();
+  };
+
   it('shows error message and disabled button because no search criteria are typed', () => {
-    //todo
+    setup();
+
+    cy.get('input[name=bookName]').as('bookControl').clear();
+    cy.contains('Search criteria is required!');
+
+    cy.get('@bookControl').type('kill');
+    cy.contains('Search criteria is required!').should('not.exist');
   });
 
   it('shows No book found because no book match the search', () => {
-    //todo
+    setup();
+
+    cy.get('input[name=bookName]').clear().type('sddfs');
+
+    cy.get('[data-cy="borrow-btn"]').click();
+
+    cy.contains('No book found for this search');
   });
 
   it('shows One book because the search matches one book', () => {
-    //todo
+    setup();
+
+    cy.get('input[name=bookName]').clear().type('kill');
+
+    cy.get('[data-cy="borrow-btn"]').click();
+
+    cy.get('li').should('have.length', 1);
   });
 
   it('shows One book because the search matches one book even with different cases', () => {
-    //todo
+    setup();
+
+    cy.get('input[name=bookName]').clear().type('KiLl');
+
+    cy.get('[data-cy="borrow-btn"]').click();
+
+    cy.get('li').should('have.length', 1);
   });
 
   it('shows a list of books because the search matches multiples books', () => {
-    //todo
+    setup();
+
+    cy.get('input[name=bookName]').clear().type('Tolkien');
+
+    cy.get('[data-cy="borrow-btn"]').click();
+
+    cy.get('li').should('have.length', 2);
   });
 });
