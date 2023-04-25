c29d9372c9ac5e8c1ce9d2292faf0042f71b5b45
thomas <thomas.ext@squadracer.com>
2023-04-25 16:07:40 +0200

    feat(challenge20): add solution for cypress

--- apps/testing-modal/src/app/app.component.cy.ts
+++ apps/testing-modal/src/app/app.component.cy.ts
@@ -1,19 +1,42 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 describe(AppComponent.name, () => {
-  const setup = () => {
-    cy.mount(AppComponent);
-  };
-
   test('error modal is displayed if you click on "Confirm" without inputing a name', () => {
-    setup();
+    cy.mount(AppComponent, { imports: [BrowserAnimationsModule] });
+
+    cy.contains('button', 'Confirm').click();
+
+    cy.get('mat-dialog-container').contains('h1', 'Error');
   });
 
   test('error message is shown if you click "Cancel" in the confirmation modal after submitting a name', () => {
-    setup();
+    cy.mount(AppComponent, { imports: [BrowserAnimationsModule] });
+
+    cy.get('input').type('toto');
+
+    cy.contains('button', 'Confirm').click();
+
+    cy.get('mat-dialog-container').contains('h1', 'Profil');
+    cy.get('mat-dialog-container').contains('Name: toto');
+
+    cy.contains('button', 'Cancel').click();
+
+    cy.contains('Name is invalid !!');
   });
 
   test('confirm message is shown if you click "Confirm" in the confirmation modal after submitting a name', () => {
-    setup();
+    cy.mount(AppComponent, { imports: [BrowserAnimationsModule] });
+
+    cy.get('input').type('toto');
+
+    cy.contains('button', 'Confirm').click();
+
+    cy.get('mat-dialog-container').contains('h1', 'Profil');
+    cy.get('mat-dialog-container').contains('Name: toto');
+
+    cy.contains('button', 'Confirmation').click();
+
+    cy.contains('Name has been submitted');
   });
 });
