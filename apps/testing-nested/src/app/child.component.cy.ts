cfa357083460cfdd541f78f4ca016280ec2a1720
Sebastian Haack <haack.sebastian@gmail.com>
2023-07-19 11:37:28 +0200

    feat: challenge 18 solution

--- apps/testing-nested/src/app/child.component.cy.ts
+++ apps/testing-nested/src/app/child.component.cy.ts
@@ -1,15 +1,31 @@
+import { TestBed } from '@angular/core/testing';
 import { ChildComponent } from './child.component';
+import { HttpService } from './http.service';
 
 describe(ChildComponent.name, () => {
-  const setup = () => {
-    cy.mount(ChildComponent);
-  };
+  beforeEach(() => {
+    cy.mount(ChildComponent).then(() => {
+      const http = TestBed.inject(HttpService);
+      cy.stub(http, 'sendTitle').as('http');
+    });
+  });
 
   test('add Good title and send request title with no error', () => {
-    setup();
+    cy.get('input').clear();
+    cy.get('input').type('TestTitle');
+
+    cy.get('button').click();
+
+    cy.get('p').contains('Title is TestTitle');
+
+    cy.get('@http').should('be.calledOnce');
   });
 
   test('fail validating title because no title were typed', () => {
-    setup();
+    cy.get('button').click();
+
+    cy.contains('Title is required !!!');
+
+    cy.get('@http').should('not.be.called');
   });
 });
--- apps/testing-nested/src/app/child.component.spec.ts
+++ apps/testing-nested/src/app/child.component.spec.ts
@@ -1,12 +1,51 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import { createMockWithValues } from '@testing-library/angular/jest-utils';
+import userEvent from '@testing-library/user-event';
 import { ChildComponent } from './child.component';
+import { HttpService } from './http.service';
 
 describe('ChildComponent', () => {
+  const setup = async () => {
+    const httpServiceMock = createMockWithValues(HttpService, {
+      sendTitle: jest.fn(),
+    });
+
+    await render(ChildComponent, {
+      componentProviders: [
+        {
+          provide: HttpService,
+          useValue: httpServiceMock,
+        },
+      ],
+    });
+    return { httpServiceMock };
+  };
   test('add Good title and send request title with no error', async () => {
-    await render(ChildComponent);
+    const { httpServiceMock } = await setup();
+
+    const button = screen.getByRole('button');
+    const input = screen.getByRole('textbox');
+
+    await userEvent.type(input, 'TestTitle');
+    await userEvent.click(button);
+
+    expect(screen.queryByText('Title is required !!!')).not.toBeInTheDocument();
+    expect(screen.getByText('Title is TestTitle')).toBeInTheDocument();
+
+    expect(httpServiceMock.sendTitle).toHaveBeenCalledTimes(1);
   });
 
   test('fail validating title because no title were typed', async () => {
-    await render(ChildComponent);
+    const { httpServiceMock } = await setup();
+
+    const button = screen.getByRole('button');
+    const input = screen.getByRole('textbox');
+
+    await userEvent.clear(input);
+    await userEvent.click(button);
+
+    expect(screen.queryByText('Title is required !!!')).toBeInTheDocument();
+
+    expect(httpServiceMock.sendTitle).not.toHaveBeenCalled();
   });
 });
