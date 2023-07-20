910743414b6abe26c257afc0048c05c918501a7a
Sebastian Haack <haack.sebastian@gmail.com>
2023-07-20 14:31:28 +0200

    feat: challenge 20 solution

--- apps/testing-modal/src/app/app.component.spec.ts
+++ apps/testing-modal/src/app/app.component.spec.ts
@@ -1,16 +1,67 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  test('error modal is displayed if you click on "Confirm" without inputing a name', async () => {
+  const initTestWithInput = async () => {
+    const user = userEvent.setup();
     await render(AppComponent);
+
+    const confirmControl = await screen.findByRole('button', {
+      name: /confirm/i,
+    });
+    const inputControl = await screen.findByRole('textbox');
+
+    await user.clear(inputControl);
+    await user.type(inputControl, 'sebtest');
+
+    await user.click(confirmControl);
+
+    screen.getByRole('heading', { name: /Profil/i, level: 1 });
+    screen.getByText('Name: sebtest');
+  };
+
+  test('error modal is displayed if you click on "Confirm" without inputing a name', async () => {
+    const user = userEvent.setup();
+    await render(AppComponent);
+
+    const confirmControl = await screen.findByRole('button', {
+      name: /confirm/i,
+    });
+    const inputControl = await screen.findByRole('textbox');
+
+    await user.clear(inputControl);
+
+    await user.click(confirmControl);
+
+    const dialogControl = await screen.findByRole('dialog');
+
+    expect(dialogControl).toBeInTheDocument();
+
+    screen.getByRole('heading', { name: /error/i, level: 1 });
   });
 
   test('error message is shown if you click "Cancel" in the confirmation modal after submitting a name', async () => {
-    await render(AppComponent);
+    await initTestWithInput();
+
+    const cancelControl = await screen.findByRole('button', {
+      name: /cancel/i,
+    });
+
+    await userEvent.click(cancelControl);
+
+    screen.findByText('Name is invalid !!');
   });
 
   test('confirm message is shown if you click "Confirm" in the confirmation modal after submitting a name', async () => {
-    await render(AppComponent);
+    await initTestWithInput();
+
+    const confirmationControl = await screen.findByRole('button', {
+      name: /confirm/i,
+    });
+
+    await userEvent.click(confirmationControl);
+
+    screen.findByText('Name has been submitted');
   });
 });
