192bb799444d3fda7ff201065c3688cff9dbea9b
Sebastian Haack <haack.sebastian@gmail.com>
2023-07-20 10:01:38 +0200

    feat: challenge 19 solution

--- apps/testing-input-output/src/app/counter.component.cy.ts
+++ apps/testing-input-output/src/app/counter.component.cy.ts
@@ -1,11 +1,58 @@
+import { createOutputSpy } from 'cypress/angular';
 import { CounterComponent } from './counter.component';
 
 describe(CounterComponent.name, () => {
   it('using createOutputSpy', () => {
-    cy.mount(CounterComponent);
+    cy.mount(CounterComponent, {
+      componentProperties: {
+        initialValue: 5,
+        send: createOutputSpy<boolean>('sendSpy'),
+      },
+    });
+
+    cy.get('[data-testid="counter"]').as('counter').should('contain', 5);
+
+    cy.get('button').contains('Increment').click().click().click().click();
+
+    cy.get('@counter').should('contain', 9);
+
+    cy.get('button').contains('Send').click();
+
+    cy.get('@sendSpy').should('have.been.calledOnce');
+    cy.get('@sendSpy').should('have.been.calledWith', 9);
+
+    cy.get('button').contains('Decrement').click().click();
+
+    cy.get('button').contains('Send').click();
+
+    cy.get('@sendSpy').should('have.been.calledTwice');
+    cy.get('@sendSpy').should('have.been.calledWith', 7);
   });
 
   it('using autoSpyOutputs', () => {
-    cy.mount(CounterComponent);
+    cy.mount(CounterComponent, {
+      autoSpyOutputs: true,
+      componentProperties: {
+        initialValue: 5,
+      },
+    });
+
+    cy.get('[data-testid="counter"]').as('counter').should('contain', 5);
+
+    cy.get('button').contains('Increment').click().click().click().click();
+
+    cy.get('@counter').should('contain', 9);
+
+    cy.get('button').contains('Send').click();
+
+    cy.get('@sendSpy').should('have.been.calledOnce');
+    cy.get('@sendSpy').should('have.been.calledWith', 9);
+
+    cy.get('button').contains('Decrement').click().click();
+
+    cy.get('button').contains('Send').click();
+
+    cy.get('@sendSpy').should('have.been.calledTwice');
+    cy.get('@sendSpy').should('have.been.calledWith', 7);
   });
 });
